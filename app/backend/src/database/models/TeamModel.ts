dbfc72548032252ea7367278796ef5b064190526
Glaucoed <gesverzuthe@gmail.com>
2023-02-27 17:19:13 -0300

    feat: realizado requisito 01

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,26 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from './index';
+
+export default class Team extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Team.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'teams',
+
+});
