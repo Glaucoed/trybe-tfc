a9b194802d5a9ff77bcb3d365b3e728c28217235
Glaucoed <gesverzuthe@gmail.com>
2023-02-27 21:27:45 -0300

    feat: realizado requisito 04

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -17,7 +17,7 @@ describe('Teste de integração da teams', function () {
     sinon.restore();
   });
 
-  it('Deve retornar uma lista de times e o status 200', async function () {
+  it('Deve retornar uma lista de times e o status 200 utilizando a rota /teams', async function () {
     const outputMock = [
       {
         "id": 1,
@@ -30,6 +30,18 @@ describe('Teste de integração da teams', function () {
     ];
 
     const response = await chai.request(app).get('/teams').send(outputMock);
+
+    expect(response.status).to.be.equal(200);
+
+  });
+
+  it('Deve retornar uma lista de times e o status 200 utilizando a rota /teams/5', async function () {
+    const outputMock = {
+      "id": 5,
+      "teamName": "Cruzeiro"
+    };
+
+    const response = await chai.request(app).get('/teams/5').send(outputMock);
 
     expect(response.status).to.be.equal(200);
 
