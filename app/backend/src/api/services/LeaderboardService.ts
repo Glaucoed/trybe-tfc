afa006530cdc4456e7d806200a1f883386c1e807
Glaucoed <gesverzuthe@gmail.com>
2023-03-03 09:27:40 -0300

    feat: realizado requisito 24 e 25

--- app/backend/src/api/services/LeaderboardService.ts
+++ app/backend/src/api/services/LeaderboardService.ts
@@ -6,38 +6,39 @@ class LeaderboardService {
   protected matchesModel: ModelStatic<MatchesModel> = MatchesModel;
   protected teamModel: ModelStatic<TeamModel> = TeamModel;
 
-  private _name = literal('homeTeam.team_name');
-  private _totalPoints = literal(`CAST((SUM(home_team_goals > away_team_goals) * 3) +
-  SUM(home_team_goals = away_team_goals) AS UNSIGNED)`);
-
-  private _totalGames = fn('COUNT', col('home_team_id'));
-  private _totalVictories = literal('CAST(SUM(home_team_goals > away_team_goals) AS UNSIGNED)');
-  private _totalDraws = literal('CAST(SUM(home_team_goals = away_team_goals) AS UNSIGNED)');
-  private _totalLosses = literal('CAST(SUM(away_team_goals > home_team_goals) AS UNSIGNED)');
-  private _goalsFavor = literal('CAST(SUM(home_team_goals) AS UNSIGNED)');
-  private _goalsOwn = literal('CAST(SUM(away_team_goals) AS UNSIGNED)');
-  private _goalsBalance = literal('CAST(SUM(home_team_goals - away_team_goals) AS SIGNED )');
-  private _efficiency = literal(`ROUND((CAST((SUM(home_team_goals > away_team_goals) * 3) +
-  SUM(home_team_goals = away_team_goals) AS UNSIGNED) /
-  (COUNT(home_team_id) * 3)) * 100, 2)`);
+  private query = {
+    name: literal('homeTeam.team_name'),
+    totalPoints: literal(`CAST((SUM(home_team_goals > away_team_goals) * 3) +
+    SUM(home_team_goals = away_team_goals) AS UNSIGNED)`),
+    totalGames: fn('COUNT', col('home_team_id')),
+    totalVictories: literal('CAST(SUM(home_team_goals > away_team_goals) AS UNSIGNED)'),
+    totalDraws: literal('CAST(SUM(home_team_goals = away_team_goals) AS UNSIGNED)'),
+    totalLosses: literal('CAST(SUM(away_team_goals > home_team_goals) AS UNSIGNED)'),
+    goalsFavor: literal('CAST(SUM(home_team_goals) AS UNSIGNED)'),
+    goalsOwn: literal('CAST(SUM(away_team_goals) AS UNSIGNED)'),
+    goalsBalance: literal('CAST(SUM(home_team_goals - away_team_goals) AS SIGNED )'),
+    efficiency: literal(`ROUND((CAST((SUM(home_team_goals > away_team_goals) * 3) +
+    SUM(home_team_goals = away_team_goals) AS UNSIGNED) /
+    (COUNT(home_team_id) * 3)) * 100, 2)`),
+  };
 
   async homeScore() {
     return this.matchesModel.findAll({
       include: [{ model: TeamModel, as: 'homeTeam', attributes: [] }],
-      attributes: [[this._name, 'name'],
-        [this._totalPoints, 'totalPoints'],
-        [this._totalGames, 'totalGames'],
-        [this._totalVictories, 'totalVictories'],
-        [this._totalDraws, 'totalDraws'],
-        [this._totalLosses, 'totalLosses'],
-        [this._goalsFavor, 'goalsFavor'],
-        [this._goalsOwn, 'goalsOwn'],
-        [this._goalsBalance, 'goalsBalance'],
-        [this._efficiency, 'efficiency']],
+      attributes: [[this.query.name, 'name'],
+        [this.query.totalPoints, 'totalPoints'],
+        [this.query.totalGames, 'totalGames'],
+        [this.query.totalVictories, 'totalVictories'],
+        [this.query.totalDraws, 'totalDraws'],
+        [this.query.totalLosses, 'totalLosses'],
+        [this.query.goalsFavor, 'goalsFavor'],
+        [this.query.goalsOwn, 'goalsOwn'],
+        [this.query.goalsBalance, 'goalsBalance'],
+        [this.query.efficiency, 'efficiency']],
       where: { inProgress: false },
       group: ['home_team_id'],
-      order: [['totalVictories', 'DESC'], ['goalsBalance', 'DESC'], ['goalsFavor', 'DESC'],
-        ['goalsOwn', 'DESC'],
+      order: [['totalPoints', 'DESC'], ['totalVictories', 'DESC'], ['goalsBalance', 'DESC'],
+        ['goalsFavor', 'DESC'], ['goalsOwn', 'DESC'],
       ],
     });
   }
